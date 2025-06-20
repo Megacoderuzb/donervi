d0b9265241beb06d2d2da8a77345d13fbf5ab0c4
Megacoderuzb <megacoderuzb@gmail.com>
2025-06-20 16:26:16 +0500

    fix: main ts port change

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors();
   app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3005);
+  await app.listen(4000, '0.0.0.0');
 }
 
 bootstrap();
